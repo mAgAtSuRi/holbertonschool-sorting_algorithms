0908fa8c731a52ab009a1e53188406e97c40b12a
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-08-06 11:15:22 +0200

    first try

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bublle_sort - sort int array with Bubble sort algorithm
+ * -> go through array and swap 2 elements for every step
+ * https://www.youtube.com/watch?v=lyZQPjUT5B4&t=306s
+ *
+ * @array: array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (size <= 1)
+		return;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+			}
+		}
+		print_array(array, size);
+	}
+}
