24f58769e955bfede25f2aea9cdbaf3fe0cc7dca
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-08-06 16:53:45 +0200

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -32,6 +32,7 @@ void swap(listint_t **list, listint_t *node_prev, listint_t *node)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *node, *next_node;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
