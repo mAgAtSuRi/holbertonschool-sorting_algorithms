723e59b9da9ff6e8702356867d126ca5afc432f4
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-08-06 16:52:41 +0200

    added a node next_node to store value

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -31,18 +31,19 @@ void swap(listint_t **list, listint_t *node_prev, listint_t *node)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
+	listint_t *node, *next_node;
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	node = (*list)->next;
 	while (node != NULL)
 	{
+		next_node = node->next;
 		while (node->prev != NULL && node->n < node->prev->n)
 		{
 			swap(list, node->prev, node);
-			node = node->prev;
+			print_list(*list);
 		}
-		node = node->next;
+		node = next_node;
 	}
 }
