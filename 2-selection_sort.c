6fa80b2e8335069ce1c672fcd1b1e58769607fc2
Tristan <tristan.latuilliere@gmail.com>
2025-08-07 11:13:52 +0200

    new_def of selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,19 +7,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i, j, temp, min_index;
 
 	for (i = 0; i < size; i++)
 	{
+		min_index = i;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
+			if (array[min_index] > array[j])
+					min_index = j;
+		}
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
 		}
 	}
 }
