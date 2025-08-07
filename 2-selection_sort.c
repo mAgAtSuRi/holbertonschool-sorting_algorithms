baa164ae3efcb84931c67ab3ab36741bda03f471
Tristan <tristan.latuilliere@gmail.com>
2025-08-07 11:15:31 +0200

    betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,7 +3,7 @@
 /**
  * selection_sort - sorts an array using selection sort algorithm
  * @array: array to check
- * size: size of the array
+ * @size: size of the array
  */
 void selection_sort(int *array, size_t size)
 {
